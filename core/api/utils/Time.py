ded73e99c66c3b44134f19564a94b61d33ade0c6
Anirudha Bose <ani07nov@gmail.com>
2016-04-17 13:35:41 +0530

    API v3

--- core/api/utils/Time.py
+++ core/api/utils/Time.py
@@ -1,7 +1,11 @@
 from email.utils import formatdate
 from time import time
-from datetime import tzinfo
+from datetime import tzinfo, datetime
 
 
 def get_current_local_time():
     return formatdate(time(), tzinfo())
+
+
+def timestamp_to_obj(timestamp):
+    return datetime.fromtimestamp(timestamp)
